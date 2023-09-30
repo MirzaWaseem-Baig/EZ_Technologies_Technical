9fe50c5010246c043f02506d06bc28e94de3e51f
Mirza Waseem Baig <146192961+MirzaWaseem-Baig@users.noreply.github.com>
2023-09-30 18:15:56 +0530

    Create CloseMissing.py

---
+++ Day-3/CloseMissing.py
@@ -0,0 +1,12 @@
+s=input()
+l=[int(x) for x in s.split(" ")]
+l1=[]
+for x in l:
+    if x>=0:
+        l1.append(x)
+l1.sort()
+for i in range(0,len(s)):
+    if(i!=l1[i]):
+        print(i)
+        break
+print(max(l1)+1)
