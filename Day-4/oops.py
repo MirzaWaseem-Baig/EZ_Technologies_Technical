862530db2450e34b76f08a5d762c653804963289
Mirza Waseem Baig <146192961+MirzaWaseem-Baig@users.noreply.github.com>
2023-10-04 21:39:06 +0530

    Create oops.py

---
+++ Day-4/oops.py
@@ -0,0 +1,11 @@
+class A:
+    x=int(input())
+    y=input()
+    z=int(input())
+    def first(y,x):
+        print(y.reverse())
+        print(x*x)
+    def displayresults():
+        print(len(y))
+        print(x%z)
+    
