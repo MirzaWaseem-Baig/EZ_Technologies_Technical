dc55c1045c88a0efcbe968e5c6fb355e9239e266
Mirza Waseem Baig <146192961+MirzaWaseem-Baig@users.noreply.github.com>
2023-10-18 19:21:15 +0530

    Create Patterns.py

---
+++ Day-16/Patterns.py
@@ -0,0 +1,74 @@
+n = int(input())
+'''row=n
+col=2*n-1
+start,end=n-1,n-1
+for i in range(row):
+    for j in range(col):
+       if j>=start and j<=end:
+           print("*",end="")
+       else:
+            print(" ",end="")
+    print()
+    start-=1
+    end+=1'''
+'''row = n
+col = n
+for i in range(row):
+    for j in range(col):
+        if(i==j) or (i+j) == n-1:
+            print(" ", end=" ")
+        elif i==0 or i==n-1 or j==0 or j==n-1:
+            print("*",end=" ") 
+        else:
+            print(" ", end=" ")
+    print()'''
+'''char = 'a'
+for i in range(n):
+    for j in range(i+1):
+        print(char,end=" ")
+        char = chr(ord(char)+1)
+        if char > "z":
+            char = "a"
+    print()'''
+'''for i in range(1,n+1):
+    print(" "*(n-i),end=" ")
+    for j in range(1,i):
+        print("*",end=" ")
+    print()'''
+'''row=n
+col=2*n-1
+var=2
+start,end=n-1,n-1
+for i in range(row):
+    for j in range(col):
+        if (j==start or j==end) and i!=n-1:
+            print("*",end="")
+        elif i == n-1 and (j>=start and j<=n-1):
+            print("*",end=" ")
+        else:
+            print(" ",end="")
+    print()
+    start-=1
+    end+=1'''
+row=n
+col=2*n-1
+var=2
+var2=1
+start,end=n-1,n-1
+for i in range(row):
+    for j in range(col):
+        if (j==start or j==end) and i!=n-1:
+            if start==end:
+                print(1,end="")
+            elif j==start:
+                print(1,end=" ")
+            else:
+                print(var,end=" ")
+                var+=1
+        elif i == n-1 and (j>=start and j<=n-1):
+            print(var2,end=" ")
+        else:
+            print(" ",end="")
+    print()
+    start-=1
+    end+=1
